483b663996a17f640ac6b6c18500017f5390b47f
Andrew Sprague <andrewsprague@outlook.com>
2018-08-08 21:08:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz (number)
   if (number % 3 == 0 && number % 5 == 0)
-    return "Fizzbuzz"
+    return "FizzBuzz"
   elsif (number % 3 == 0)
     return "Fizz"
   elsif (number % 5 == 0)
